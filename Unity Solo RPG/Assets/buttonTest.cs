216234b2fe9450c9af0cc048882bb3b2447b4ea8
Julien Leseine <leseine.julien.logiciel@gmail.com>
2014-11-07 08:25:36 +0100

    Test1.1
    le test 1 est un test destiné à vérifier que les répository settings
    sont correctement définies pour git

---
+++ Unity Solo RPG/Assets/buttonTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ButtonTest(string str)
+    {
+        Debug.Log(str);
+    }
+}
