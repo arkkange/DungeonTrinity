b09ce62d7c5a77306ccac962daf3eb5dacebff30
Julien Leseine <leseine.julien.logiciel@gmail.com>
2014-11-12 18:07:05 +0100

    Création du projet
    Il ya trois scène destinée au démarrage du projet.
    Pensez bien à créer une nouvelle scène pour faire vos tests et créer vos
    préfabs.

---
+++ DungeonTrinity/Assets/Scripts/TimeLine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeLine : MonoBehaviour {
+
+   private int                  _maxTime;
+   private int                  _timeSpend;
+   private ArrayList<Skills>    _ActionsList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
