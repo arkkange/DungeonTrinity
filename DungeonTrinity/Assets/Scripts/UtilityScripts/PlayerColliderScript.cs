66de04a4f51b860fa1fb03e54f93e915c4824290
Julien Leseine <leseine.julien.logiciel@gmail.com>
2014-11-16 18:04:35 +0100

    Warrior Creation alpha 1.1

---
+++ DungeonTrinity/Assets/Scripts/UtilityScripts/PlayerColliderScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerColliderScript : MonoBehaviour {
+
+    List<Transform>     _TransformListOfCollisions = new List<Transform>();
+
+    /***********************************************************\
+    |   OnTriggerEnter : recupère la liste des collisions       |
+    \***********************************************************/
+    void OnTriggerEnter(Collider other)
+    {
+        _TransformListOfCollisions.Add(other.transform);
+    }
+
+    /***********************************************************\
+    |   OnTriggerEnter : recupère la liste des collisions       |
+    \***********************************************************/
+    void OnTriggerExit(Collider other)
+    {
+        _TransformListOfCollisions.Remove(other.transform);
+    }
+
+    /***********************************************************************\
+    |   GetListOfCollisions : Donne la liste des objets dans le collider    |
+    \***********************************************************************/
+    List<Transform> GetListOfCollisions()
+    {
+        return _TransformListOfCollisions;
+    }
+
+}
