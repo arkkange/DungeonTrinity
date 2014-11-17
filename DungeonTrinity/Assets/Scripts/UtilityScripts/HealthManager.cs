72a632f45a1cb20c64622d83f3ca87b9ee78d3a6
Julien Leseine <leseine.julien.logiciel@gmail.com>
2014-11-17 01:13:50 +0100

    Warrior creation alpha 1.3

---
+++ DungeonTrinity/Assets/Scripts/UtilityScripts/HealthManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+    public int _ActualLife;
+    public int _MaxLife;
+
+    public bool _invincible = false;
+    public bool _isDead = false;
+
+    void start()
+    {
+        _ActualLife = _MaxLife;
+    }
+
+    void damage(int value)
+    {
+        int newLife = _ActualLife - value;
+        if (newLife < 0)
+        {
+            _ActualLife = 0;
+            _isDead = true;
+        }
+        if (newLife > _MaxLife)
+        {
+            _ActualLife = _MaxLife;
+        }
+    }
+
+}
